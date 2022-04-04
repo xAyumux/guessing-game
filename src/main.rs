b1f5a6fd76c17659b0e5abdc253cd272d1017ffd
xAyumux <ayumu.f.1127@gmail.com>
2022-04-05 01:20:24 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the number!"); // 数を当ててごらん
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number); // 秘密の数字は次の通り: {}
+
+    println!("Please input your guess."); // ほら、予想を入力してね
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Faild to read line"); // 行の読み込みに失敗しました
+
+    println!("You guessed: {}", guess); // 次のように予想しました: {}
+}
